b6eea15e53fdcba6d3f225fa0423544e555b5f02
Odin <itzmeeldhobaby@gmail.com>
2025-04-05 15:22:20 +0530

    fx

--- setup.sh
+++ setup.sh
@@ -2,4 +2,4 @@
 wget $( curl https://api.github.com/repos/revanced/revanced-cli/releases | jq -r '.[0].assets[] | select(.name | test("revanced-cli.*\\.jar$")) | .browser_download_url' ) -O cli.jar
 
 # Downloads patches
-wget $(cat releases.json | jq -r '.assets[] | select(.name | test("patches.*\\.rvp$")) | .browser_download_url') -O patches.rvp
+wget $(cat release.json | jq -r '.assets[] | select(.name | test("patches.*\\.rvp$")) | .browser_download_url') -O patches.rvp
