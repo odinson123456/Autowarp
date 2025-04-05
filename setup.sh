39d78d4aef091c0fad808e0030b5c2246cc0f2fa
Odin <itzmeeldhobaby@gmail.com>
2025-04-05 16:34:51 +0530

    fx

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 # Downloads cli
-wget $( curl https://api.github.com/repos/revanced/revanced-cli/releases | jq -r '.[0].assets[] | select(.name | test("revanced-cli.*\\.jar$")) | .browser_download_url' ) -O cli.jar
+wget -q $( curl https://api.github.com/repos/revanced/revanced-cli/releases | jq -r '.[0].assets[] | select(.name | test("revanced-cli.*\\.jar$")) | .browser_download_url' ) -O cli.jar
 
 # Downloads patches
-wget $(cat release.json | jq -r '.assets[] | select(.name | test("patches.*\\.rvp$")) | .browser_download_url') -O patches.rvp
+wget -q $(cat release.json | jq -r '.assets[] | select(.name | test("patches.*\\.rvp$")) | .browser_download_url') -O patches.rvp
 
 # writes commit body to data.txt
 jq -r '.body' release.json > data.txt
